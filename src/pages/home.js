d018a1721ea724ed1e500f95bd9491334d0376cf
Thomas Williams <thomas.williams29899@gmail.com>
2022-07-16 23:28:57 +0100

    fixed pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,11 +6,11 @@ import "./home.css"
 const Home = () => {
     return (
         <>
-        <div className="night_home">
-            <Background/>
-        </div>
             <div className="main_home">
                 <div className="page_home">
+                    <div className="night_home">
+                        <Background/>
+                    </div>
                     <h1 className="name_home">Thomas Lee<br></br>Williams</h1>
                     <h2 className="title_home">Welcome to my personal website</h2>
                     <Columns />
