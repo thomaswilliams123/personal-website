c932598a66a299124df623fa21cc726b432b4ca1
Thomas Williams <thomas.williams29899@gmail.com>
2022-07-22 04:54:41 +0100

    new about and projects page

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -10,8 +10,26 @@ const Projects = () => {
             <Header/>
                 <div className="page_projects">
                     <h1 className="name_projects">Projects</h1>
-                    <div>
+                    <div className="weather-app">
                         <Weather/>
+                        <div className="weather-app-desc">
+                            <h2 className="weather-app-title">Weather Forcast App</h2>
+                            <p className="weather-app-text">This component is a weather
+                            app built to not only get the current tempreture of a given
+                            location, but also the next 4 days forecast weather for the 
+                            given location.
+                            <br></br>
+                            <br></br>
+                            The component does this by calling the OpenWeather current
+                            weather API to get the current weather for the city, county,
+                            state (US only), town, and country entered into the search bar.
+                            The exact same happens with the 5 day / 3 hour forecast API.
+                            <br></br>
+                            <br></br>
+                            It then displays the current forecast with the next 4 days 
+                            forecast in a easy to see format. 
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
